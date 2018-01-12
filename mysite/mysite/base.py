09fe3f79892d83862bac91cd5b58725df7759f12
igorhf <igorhf.2@gmail.com>
2018-01-12 13:21:05 -0200

    criado arquivo base
    falta criar a url e a views

---
+++ mysite/mysite/base.py
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Django</title>
+	<meta charset="utf-8">
+</head>
+<body>
+<div class="div_master">
+    <!--menu-->
+    <nav class="menu">
+	    <ol>
+		    <li><a href="">Home</a></li>
+		    <li><a href="">XBOX</a></li>
+		    <li><a href="">PS</a></li>
+		    <li><a href="">PC</a></li>
+		    <li><a href="">NINTENDO</a></li>
+	    </ol>
+    </nav>
+    <!---->
+
+      
+
+
+</div>
+</body>
+</html>
